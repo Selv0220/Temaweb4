edc7ee2aca701fa226f2162d00d463478964d91f
selvanur <selv0220>
2021-03-02 22:49:36 +0100

    Ny kode

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <meta name="copyright" content="Information om copyright">
 
     <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.scss" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
     <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -33,31 +33,31 @@
 
 <div class="boks1">
     <div class="items">
-        <a href="index.html" style="text-decoration: none;"> <img src="images/logo.png"> </a>
+        <a href="index.php" style="text-decoration: none;"> <img src="images/logo.png"> </a>
     </div>
     <div class="items1">
-        <a href=" Advarsel.html" style="text-decoration: none;"><i class="fas fa-circle" style="color: red"></i>Advarsel:Snestorm</a>
+        <a href=" Advarsel.php" style="text-decoration: none;"><i class="fas fa-circle" style="color: red"></i>Advarsel:Snestorm</a>
     </div>
 
     <div class="items2">
-        <a href="Information.html" style="text-decoration: none;">Information</a>
+        <a href="Information.php" style="text-decoration: none;">Information</a>
     </div>
 
     <div class="items3">
-        <a href="Vejrkort.html" style="text-decoration: none;">Vejrkort</a>
+        <a href="Vejrkort.php" style="text-decoration: none;">Vejrkort</a>
     </div>
 
     <div class="items4">
-        <a href="Varsler.html" style="text-decoration: none;">Varsler</a>
+        <a href="Varsler.php" style="text-decoration: none;">Varsler</a>
     </div>
 
     <div class="items5">
-        <a href="Søg.html" style="text-decoration: none;">Søg...<i class="fas fa-search" style="color: black"></i></a>
+        <a href="Søg.php" style="text-decoration: none;">Søg...<i class="fas fa-search" style="color: black"></i></a>
     </div>
 </div>
 
 <div class="items6">
-    <a1 href="Vælgby.html" style="text-decoration: none;"> Vælg by... <i class="fas fa-arrow-down"></i></a1>
+    <a1 href="Vælgby.php" style="text-decoration: none;"> Vælg by... <i class="fas fa-arrow-down"></i></a1>
 </div>
 
 <div class="boks2">
@@ -84,7 +84,7 @@
     </div>
 
     <div class="foot5">
-        <div> <a href="index.html" style="text-decoration: none;"> <img src="images/logo.png"> </a></div>
+        <div> <a href="index.php" style="text-decoration: none;"> <img src="images/logo.png"> </a></div>
     </div>
 </div>
 
