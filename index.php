d64917e87e33d16098bae2cac5d579eb8aa63d2a
selvanur <selv0220>
2021-03-04 12:09:09 +0100

    Forside opdatering - Header og footer

--- index.php
+++ index.php
@@ -17,76 +17,102 @@
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
-    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
     <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <style>
     body{
-        background-image: url("images/Baggrund.jpg");
+            background-image: url("images/Baggrund.jpg");
     }
 </style>
 
 <body>
 
-<div class="boks1">
-    <div class="items">
-        <a href="index.php" style="text-decoration: none;"> <img src="images/logo.png"> </a>
-    </div>
-    <div class="items1">
-        <a href=" Advarsel.php" style="text-decoration: none;"><i class="fas fa-circle" style="color: red"></i>Advarsel:Snestorm</a>
-    </div>
 
-    <div class="items2">
-        <a href="Information.php" style="text-decoration: none;">Information</a>
-    </div>
+        <!--Hvis mellerum øsnkes mellem boksene så brug g efter row
+        Du kan vælge gy (ned ad) gx(til siden)
+        Span ændre farve på tekst
 
-    <div class="items3">
-        <a href="Vejrkort.php" style="text-decoration: none;">Vejrkort</a>
-    </div>
+        ved mobil col-12(1 boks, resten under)
+        ved mellem col-md-6(2 ved siden af hinanden, resten under)
+        ved stor col-xl-2(6 ved siden af hinanden)
 
-    <div class="items4">
-        <a href="Varsler.php" style="text-decoration: none;">Varsler</a>
-    </div>
+        hvis jeg ønsker mellemrum mellem boksene:
+        <div class="col">
+        <div class=bg-primary">Test</div> -->
 
-    <div class="items5">
-        <a href="Søg.php" style="text-decoration: none;">Søg...<i class="fas fa-search" style="color: black"></i></a>
-    </div>
-</div>
+        <!--HEADER -->
+        <div class="container mt-3 p-3 rounded-pill bg-HFColor">
+            <div class="row justify-content-center"">
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="index.php" style="text-decoration: none;"><img src="images/logo.png"></a>
+                </div>
 
-<div class="items6">
-    <a1 href="Vælgby.php" style="text-decoration: none;"> Vælg by... <i class="fas fa-arrow-down"></i></a1>
-</div>
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="Advarsel.php" style="text-decoration: none;" class="link-Hvid border border-2 border-primary rounded-pill rounded-2"><i class="fas fa-circle" style="color: red"></i>Advarsel:Snestorm</a>
+                </div>
 
-<div class="boks2">
-    <div class="items7">
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="Information.php" style="text-decoration: none;" class="link-Hvid">Information</a>
+                </div>
 
-    </div>
-</div>
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="Vejrkort.php" style="text-decoration: none;" class="link-Hvid">Vejrkort</a>
+                </div>
 
-<div class="footer">
-    <div class="foot1">
-        <div> Presse og medier </div>
-    </div>
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="Varsler.php" style="text-decoration: none;" class="link-Hvid">Varsler</a>
+                </div>
 
-    <div class="foot2">
-        <div> Job og karriere </div>
-    </div>
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="Søg.php" style="text-decoration: none;" class="link-Hvid border border-2 border-primary rounded-pill">Søg...<i class="fas fa-search" style="color: black"></i></a>
+                </div>
+            </div>
+        </div>
 
-    <div class="foot3">
-        <div> Om DMI </div>
-    </div>
+        <div class="container rounded-pill border border-2 border-primary m-3">
+            <div class="row">
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="Vælgby.php" style="text-decoration: none;" class="link-primary"> Vælg by... <i class="fas fa-arrow-down"></i></a>
+                </div>
+            </div>
+        </div>
 
-    <div class="foot4">
-        <div> Kontakt </div>
-    </div>
+        <div class="container m-sm-1">
+            <div class="row">
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="Vejrkort.php" style="text-decoration: none;"><img src="images/Temp1.png"></a>
+                </div>
+            </div>
+        </div>
 
-    <div class="foot5">
-        <div> <a href="index.php" style="text-decoration: none;"> <img src="images/logo.png"> </a></div>
-    </div>
-</div>
+        <!--FOOTER-->
+        <div class="container bg-HFColor">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="Presseogmedier.php" style="text-decoration: none;" class="link-Hvid">Presse og medier</a>
+                </div>
 
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="Jobogkarriere.php" style="text-decoration: none;" class="link-Hvid">Job og karriere</a>
+                </div>
+
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="OmDMI.php" style="text-decoration: none;" class="link-Hvid">Om DMI</a>
+                </div>
+
+                <div class="col-12 col-md-6 col-xl-2">
+                    <a href="Kontakt.php" style="text-decoration: none;" class="link-Hvid">Kontakt</a>
+                </div>
+
+                <div class="col-12 col-md- col-xl-3">
+                    <a href="index.php" style="text-decoration: none;"><img src="images/logo.png"></a>
+                </div>
+            </div>
+        </div>
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
